9b7f7ce6548bf6fbc66d27dd7739e6c2c0dd624a
EC2 Default User <ec2-user@ip-172-31-72-119.ec2.internal>
2018-02-24 03:11:00 +0000

    20180224-2nd Commit

--- server/main.js
+++ server/main.js
@@ -7,3 +7,24 @@ Meteor.startup(() => {
       password: '6e87457e3b97d1d6336922c638dcdbd6'
     });
 });
+
+
+Meteor.methods({
+    sendEmail: function (mailFields) {
+        console.log("about to send email...");
+        check([mailFields.to, mailFields.from, mailFields.subject, mailFields.text, mailFields.html], [String]);
+
+        // Let other method calls from the same client start running,
+        // without waiting for the email sending to complete.
+        this.unblock();
+
+        Meteor.Mailgun.send({
+            to: mailFields.to,
+            from: mailFields.from,
+            subject: mailFields.subject,
+            text: mailFields.text,
+            html: mailFields.html
+        });
+        console.log("email sent!");
+    }
+  });
